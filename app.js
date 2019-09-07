fbccb018ae46d8e8e27090dd134e249be0b919e4
RickyplusDartsequalPoor <16zbarham@wakatipu.school.nz>
2019-09-07 22:36:14 +1200

    v2.7 Finialised Javascript of menu and selection area

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const misc_div = document.getElementById("misc");
+const misc_div = document.getElementById("menu_misc");
 const food_div = document.getElementById("menu_food");
-const tools_div = document.getElementById("tools");
-const combat_div = document.getElementById("combat");
+const tools_div = document.getElementById("menu_tools");
+const combat_div = document.getElementById("menu_combat");
+
+const site_selected = document.getElementById("site-selected");
+const site_selection = document.getElementById("site-selection");
 
 
 function main() {
@@ -9,29 +12,95 @@ function main() {
   food_div.addEventListener('click', ()=> swap("food"));
   tools_div.addEventListener('click', ()=> swap("tools"));
   combat_div.addEventListener('click', ()=> swap("combat"));
+
+
 }
 
 function swap(menu_selection) {
-  const menuSwap = document.getElementById('swap');
+  const misc = document.getElementById("misc");
+  const food = document.getElementById("food");
+  const tools = document.getElementById("tools");
+  const combat = document.getElementById("combat");
 
   if (menu_selection === "misc") {
-    menuSwap.innerHTML = '<img src="img-misc/bamboo.png" alt="Minecraft Bamboo">';
+    misc.style.display = 'initial';
+    food.style.display = 'none';
+    tools.style.display = 'none';
+    combat.style.display = 'none';
+
+    misc_div.style.backgroundColor = '#388E3C';
+    misc_div.style.borderBottom = 'none';
+
+    // There must be a more efficent way to reset
+    food_div.style.backgroundColor = '#4CAF50';
+    food_div.style.borderBottom = '2px solid black';
+
+    tools_div.style.backgroundColor = '#4CAF50';
+    tools_div.style.borderBottom = '2px solid black';
+
+    combat_div.style.backgroundColor = '#4CAF50';
+    combat_div.style.borderBottom = '2px solid black';
+
   }
 
   if (menu_selection === "food") {
-    // menuSwap.innerHTML = '<img src="img-food/apple.png" alt="Minecraft Apple">';
-    const food = document.getElementById("food");
-    menuSwap.HTMLElementObject.innerhtml = 'food';
+    misc.style.display = 'none';
+    food.style.display = 'initial';
+    tools.style.display = 'none';
+    combat.style.display = 'none';
+
+    misc_div.style.backgroundColor = '#4CAF50';
+    misc_div.style.borderBottom = '2px solid black';
+
+    food_div.style.backgroundColor = '#388E3C';
+    food_div.style.borderBottom = 'none';
+
+    tools_div.style.backgroundColor = '#4CAF50';
+    tools_div.style.borderBottom = '2px solid black';
+
+    combat_div.style.backgroundColor = '#4CAF50';
+    combat_div.style.borderBottom = '2px solid black';
+
   }
 
   if (menu_selection === "tools") {
-    menuSwap.innerHTML = '<img src="img-tools/clock.png" alt="Minecraft Clock">';
-    menuSwap.innerHTML = '<img src="img-tools/compass.png" alt="Minecraft Compass">';
+    misc.style.display = 'none';
+    food.style.display = 'none';
+    tools.style.display = 'initial';
+    combat.style.display = 'none';
+
+    misc_div.style.backgroundColor = '#4CAF50';
+    misc_div.style.borderBottom = '2px solid black';
+
+    food_div.style.backgroundColor = '#4CAF50';
+    food_div.style.borderBottom = '2px solid black';
+
+    tools_div.style.backgroundColor = '#388E3C';
+    tools_div.style.borderBottom = 'none';
+
+    combat_div.style.backgroundColor = '#4CAF50';
+    combat_div.style.borderBottom = '2px solid black';
   }
 
   if (menu_selection === "combat") {
-    menuSwap.innerHTML = '<img src="img-combat/arrow.png" alt="Minecraft Arrow">';
+    misc.style.display = 'none';
+    food.style.display = 'none';
+    tools.style.display = 'none';
+    combat.style.display = 'initial';
+
+    misc_div.style.backgroundColor = '#4CAF50';
+    misc_div.style.borderBottom = '2px solid black';
+
+    food_div.style.backgroundColor = '#4CAF50';
+    food_div.style.borderBottom = '2px solid black';
+
+    tools_div.style.backgroundColor = '#4CAF50';
+    tools_div.style.borderBottom = '2px solid black';
+
+    combat_div.style.backgroundColor = '#388E3C';
+    combat_div.style.borderBottom = 'none';
   }
 }
+
 
 main()
